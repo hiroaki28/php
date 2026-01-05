4c7f787c1e5e7427969c2232c30d6810c49771fe
宏明 <h.ido8128baseball@gmail.com>
2026-01-05 17:59:23 +0900

    リモートリポジトリの変更

---
+++ src/php01/index03.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+$a = 20;
+$b = 5;
+
+echo ($a > 10 && $a < 30);
+echo "<br />";
+
+</body>
+</html>
